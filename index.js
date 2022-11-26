f401a83910a25e117c181bc5ae4678e0427927e4
nhn1998 <nhnayeem1@gmail.com>
2022-11-26 23:09:10 +0600

    remove gitignore files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,17 @@ app.use(express.json())
 
 // dbUser5
 // YNxxOM8L6Mky8r6i
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.fuly7hk.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
     res.send('api is running')
